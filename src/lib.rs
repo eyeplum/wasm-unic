dd1a745be14043befcacfa53fb0d81f2dfa1bcee
Yan Li <eyeplum@gmail.com>
2019-07-03 21:00:52 +1200

    Wrap unic::ucd::age::Age

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,41 @@
 #![warn(clippy::all)]
 
-use unic::ucd::{name::Name, version::UNICODE_VERSION};
+use unic::ucd::{age::Age, name::Name, version};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn unicode_version_major() -> u16 {
-    UNICODE_VERSION.major
+#[derive(Default)]
+pub struct Version {
+    pub major: u16,
+    pub minor: u16,
+    pub micro: u16,
+}
+
+impl Version {
+    fn new(version: version::UnicodeVersion) -> Self {
+        Version {
+            major: version.major,
+            minor: version.minor,
+            micro: version.micro,
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn unicode_version_minor() -> u16 {
-    UNICODE_VERSION.minor
+pub fn get_unicode_version() -> Version {
+    Version::new(version::UNICODE_VERSION)
 }
 
 #[wasm_bindgen]
-pub fn unicode_version_micro() -> u16 {
-    UNICODE_VERSION.micro
-}
-
-#[wasm_bindgen]
-pub fn get_unicode_name(chr: char) -> String {
+pub fn get_name(chr: char) -> String {
     Name::of(chr)
         .map(|n| n.to_string())
         .unwrap_or_else(|| "<none>".to_owned())
+}
+
+#[wasm_bindgen]
+pub fn get_age(chr: char) -> Version {
+    Age::of(chr)
+        .map(|age| Version::new(age.actual()))
+        .unwrap_or_default()
 }
--- www/index.js
+++ www/index.js
@@ -1,18 +1,17 @@
 import * as unic from "wasm-unic";
 
-function get_unicode_version_string() {
-  return unic.unicode_version_major() + "." +
-    unic.unicode_version_minor() + "." +
-    unic.unicode_version_micro();
+function get_version_string(version) {
+  return version.major + "." + version.minor + "." + version.micro;
 }
 
-console.log("Unicode Version: " + get_unicode_version_string());
+console.log("Unicode Version: " + get_version_string(unic.get_unicode_version()));
 
 var button = document.createElement("input");
 button.type = "button";
 button.value = "Print Unicode Name";
 button.addEventListener("click", () => {
   var userInput = document.getElementById("char-input").value;
-  console.log(unic.get_unicode_name(userInput));
+  console.log("Name : " + unic.get_name(userInput));
+  console.log(" Age : " + get_version_string(unic.get_age(userInput)));
 });
 document.body.appendChild(button);
