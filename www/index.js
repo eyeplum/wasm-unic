f1e8eb4ac07ef57d99a8b53e1d57b6d5085db0bb
Cloud <cloudcry@gmail.com>
2019-07-06 15:48:36 +0800

    feat: show results section per emoji

--- www/index.js
+++ www/index.js
@@ -1,4 +1,5 @@
 import * as unic from "wasm-unic";
+const runes = require('runes');
 
 function get_version_string(version) {
   return version.major + "." + version.minor + "." + version.micro;
@@ -10,30 +11,54 @@ function get_category_string(category) {
 
 console.log(">>>>> Unicode Version: " + get_version_string(unic.get_unicode_version()) + " <<<<<");
 
-var button = document.getElementById("button");
-var outputNode = document.getElementById("output");
-var inputNode = document.getElementById("char-input");
-button.addEventListener("click", () => {
-  var userInput = inputNode.value;
-  unic.get_segmented_scalars(userInput).forEach((value) => {
+function printCharacterResultInNode(character) {
+  var outputValue = "================== " + character + " ==================";
+  outputValue += "\n      Code Point : U+" + unic.get_code_point(character).toString(16).toUpperCase();
+  outputValue += "\n            Name : " + unic.get_name(character);
+  outputValue += "\n             Age : Unicode " + get_version_string(unic.get_age(character));
+  outputValue += "\nGeneral Category : " + get_category_string(unic.get_general_category(character));
+  outputValue += "\n==================================================";
+  console.log(outputValue);
+
+  var trNode = document.createElement("tr");
+  var codeNode = document.createElement("td");
+  codeNode.innerHTML = "U+" + unic.get_code_point(character).toString(16).toUpperCase();
+  trNode.appendChild(codeNode);
+  var cNode = document.createElement("td");
+  cNode.innerHTML = character;
+  trNode.appendChild(cNode);
+  var nameNode = document.createElement("td");
+  nameNode.innerHTML = unic.get_name(character);
+  trNode.appendChild(nameNode);
+  return trNode;
+}
+
+function printResultsForOneQueyChar(char) {
+  var tableNode = document.createElement("table");
+  unic.get_segmented_scalars(char).forEach((value) => {
     if (value < 0) {
       // This is a separator
       console.log("======= End of Grapheme Cluster =======");
       console.log("");
     } else {
-      // This is a code point
       var character = unic.get_character_from_code_point(value);
-      var outputValue = "================== " + character + " ==================";
-      outputValue += "\n      Code Point : U+" + unic.get_code_point(character).toString(16).toUpperCase();
-      outputValue += "\n            Name : " + unic.get_name(character);
-      outputValue += "\n             Age : Unicode " + get_version_string(unic.get_age(character));
-      outputValue += "\nGeneral Category : " + get_category_string(unic.get_general_category(character));
-      outputValue += "\n==================================================";
-      console.log(outputValue);
-      var pNode = document.createElement("p");
-      pNode.style = "white-space: pre-wrap;";
-      pNode.innerHTML = outputValue;
-      outputNode.appendChild(pNode);
+      tableNode.appendChild(printCharacterResultInNode(character));
     }
+  });
+  return tableNode;
+}
+
+var button = document.getElementById("button");
+var outputNode = document.getElementById("output");
+var inputNode = document.getElementById("char-input");
+button.addEventListener("click", () => {
+  outputNode.innerHTML = "";
+  var userInput = inputNode.value.trim();
+  runes(userInput).forEach((char) => { 
+    var tableNode = printResultsForOneQueyChar(char);
+    var charNode = document.createElement("p");
+    charNode.innerHTML = char;
+    outputNode.appendChild(charNode);
+    outputNode.appendChild(tableNode);
   });
 });
